b38d4594a84e754681581b35f6ffd9e6b6a92a56
Joe Moeller <moellejc@gmail.com>
2024-11-30 01:02:43 -0500

    Connect to Ring and get all cameras

--- src/functions/timerCheckRingEventsTrigger.js
+++ src/functions/timerCheckRingEventsTrigger.js
@@ -1,8 +1,16 @@
-const { app } = require('@azure/functions');
+const { app } = require("@azure/functions");
+import { RingApi } from "ring-client-api";
 
-app.timer('timerCheckRingEventsTrigger', {
-    schedule: '30 * * * * *',
-    handler: (myTimer, context) => {
-        context.log('Timer function processed request.');
-    }
+app.timer("timerCheckRingEventsTrigger", {
+  schedule: "*/30 * * * * *",
+  handler: async (myTimer, context) => {
+    context.log("Timer function processed request to check for Ring Events.");
+
+    const ringApi = new RingApi({
+      refreshToken: process.env.RING_REFRESH_TOKEN,
+    });
+
+    let devices = await ringApi.fetchRingDevices();
+    context.log(devices.allCameras);
+  },
 });
