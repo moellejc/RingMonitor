831d77b71d29672f7dee7305d77fbc1f1b16c4f3
Joe Moeller <moellejc@gmail.com>
2024-11-30 22:00:35 -0500

    Add logging statements for debug

--- src/functions/timerCheckRingEventsTrigger.js
+++ src/functions/timerCheckRingEventsTrigger.js
@@ -4,13 +4,14 @@ import { RingApi } from "ring-client-api";
 app.timer("timerCheckRingEventsTrigger", {
   schedule: "*/30 * * * * *",
   handler: async (myTimer, context) => {
-    context.log("Timer function processed request to check for Ring Events.");
-
+    context.log("Start Ring Events Check");
+    context.log("Connect to RingAPI: " + process.env.RING_REFRESH_TOKEN);
     const ringApi = new RingApi({
       refreshToken: process.env.RING_REFRESH_TOKEN,
     });
-
+    context.log("Get Ring Devices");
     let devices = await ringApi.fetchRingDevices();
     context.log(devices.allCameras);
+    context.log("End Ring Events Check");
   },
 });
